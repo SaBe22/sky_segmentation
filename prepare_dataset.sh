bd14799b6e13d7d426888deeb7f07278c741ec6a
Sabe22 <salim.belkarfa@gmail.com>
2023-04-05 12:02:15 +0200

    Fix prepare_dataset.sh script argument issue

--- prepare_dataset.sh
+++ prepare_dataset.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 INDEX_FILE_PATH=$1
-TRAIN_IMG_FOLDER=${2:''}
-TRAIN_MASK_FOLDER=${3:''}
-VAL_IMG_FOLDER=${4:''}
-VAL_MASK_FOLDER=${5:''}
-# Unzip the file
-python src/prepare_ade20k_dataset.py $INDEX_FILE_PATH --train_image_folder=$TRAIN_IMG_FOLDER --train_mask_folder=$TRAIN_MASK_FOLDER --val_image_folder=$VAL_IMG_FOLDER --val_mask_folder=$VAL_MASK_FOLDER
+TRAIN_IMG_FOLDER=${2:-''}
+TRAIN_MASK_FOLDER=${3:-''}
+VAL_IMG_FOLDER=${4:-''}
+VAL_MASK_FOLDER=${5:-''}
+# Run python script
+python src/prepare_ade20k_dataset.py "$INDEX_FILE_PATH" --train_image_folder="$TRAIN_IMG_FOLDER" --train_mask_folder="$TRAIN_MASK_FOLDER" --val_image_folder="$VAL_IMG_FOLDER" --val_mask_folder="$VAL_MASK_FOLDER"
